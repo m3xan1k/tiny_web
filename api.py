ba05683c2b49ae9e5f13e3c0246a1bb004b8c857
Shevtsov Sergey <dev.serzh@gmail.com>
2020-04-28 15:27:14 +0300

    refactor handler finder and querystring path params

--- api.py
+++ api.py
@@ -1,23 +1,31 @@
-from typing import Dict, Callable, Iterator
+from typing import Dict, Callable
 
 from webob import Request, Response
+from parse import parse
 
 
 class Api:
     def __init__(self):
         self.routes = {}
 
-    def __call__(self, environ: Dict, start_response: Callable) -> Iterator:
+    def __call__(self, environ: Dict, start_response: Callable) -> Response:
         request = Request(environ)
         response = self.handle_request(request)
         return response(environ, start_response)
 
     def handle_request(self, request: Request) -> Response:
         response = Response()
-        handler = self.routes.get(request.path)
+        handler, parsed_params = self.find_handler(request)
         if handler:
-            return handler(request, response)
+            return handler(request, response, **parsed_params)
         return self.not_found(response)
+
+    def find_handler(self, request: Request):
+        for path, handler in self.routes.items():
+            parsed_params = parse(path, request.path)
+            if parsed_params:
+                return handler, parsed_params.named
+        return None, None
 
     def route(self, path: str):
         def wrapper(handler):
